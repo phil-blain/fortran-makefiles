575136b1325158941cb7bd9394c32d92e3ee112c
Philippe Blain <levraiphilippeblain@gmail.com>
2020-03-11 11:14:47 -0400

    test.mk: correctly pass -f argument

--- test.mk
+++ test.mk
@@ -1,34 +1,37 @@
+# get the name of the makefile that was invoked on the command line
+makefile=$(firstword $(MAKEFILE_LIST))
+
 test: 
 	@echo "******* TEST: initial compile ******* "
 	$(MAKE) clean
-	$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: change mymodule implementation *******"
 	@git apply impl.patch
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: remake *******"
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: remake *******"
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: remake *******"
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: change mymodule interface ******* "
 	@git checkout -q mymodule.f90
 	@git apply interface.patch
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: remake *******"
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: remake *******"
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: remake *******"
-	@$(MAKE)
+	$(MAKE) -f $(makefile)
 	@echo "" && sleep 1
 	@echo "******* TEST: reset mymodule.f90 and make clean *******"
 	@$(MAKE) clean
